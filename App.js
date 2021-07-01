94d3c67ca757faa6074c225ebfad9f74d559274a
Bruno <info@proyectarse.com>
2021-07-01 14:56:45 -0400

    UPDATE: firebase data

--- App.js
+++ App.js
@@ -1,53 +1,47 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
-import { ThemeProvider } from "styled-components/native";
-import * as firebase from "firebase";
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ThemeProvider } from 'styled-components/native';
+import * as firebase from 'firebase';
 
 import {
-  useFonts as useOswald,
-  Oswald_400Regular,
-} from "@expo-google-fonts/oswald";
-import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+	useFonts as useOswald,
+	Oswald_400Regular,
+} from '@expo-google-fonts/oswald';
+import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
-import { theme } from "./src/infrastructure/theme";
-import { Navigation } from "./src/infrastructure/navigation";
+import { theme } from './src/infrastructure/theme';
+import { Navigation } from './src/infrastructure/navigation';
 
-import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+import { AuthenticationContextProvider } from './src/services/authentication/authentication.context';
+import firebaseConf from './src/utils/firebase-data';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA-_XRxLVmiZCbKTPHPK4tt8E1g2bZ4D7w",
-  authDomain: "mealstogo-d8374.firebaseapp.com",
-  projectId: "mealstogo-d8374",
-  storageBucket: "mealstogo-d8374.appspot.com",
-  messagingSenderId: "1980719537",
-  appId: "1:1980719537:web:f422084a80cee6ec991d90",
-};
+const firebaseConfig = firebaseConf;
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+	firebase.initializeApp(firebaseConfig);
 }
 
 export default function App() {
-  const [oswaldLoaded] = useOswald({
-    Oswald_400Regular,
-  });
+	const [oswaldLoaded] = useOswald({
+		Oswald_400Regular,
+	});
 
-  const [latoLoaded] = useLato({
-    Lato_400Regular,
-  });
+	const [latoLoaded] = useLato({
+		Lato_400Regular,
+	});
 
-  if (!oswaldLoaded || !latoLoaded) {
-    return null;
-  }
+	if (!oswaldLoaded || !latoLoaded) {
+		return null;
+	}
 
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <AuthenticationContextProvider>
-          <Navigation />
-        </AuthenticationContextProvider>
-      </ThemeProvider>
-      <ExpoStatusBar style="auto" />
-    </>
-  );
+	return (
+		<>
+			<ThemeProvider theme={theme}>
+				<AuthenticationContextProvider>
+					<Navigation />
+				</AuthenticationContextProvider>
+			</ThemeProvider>
+			<ExpoStatusBar style="auto" />
+		</>
+	);
 }
