316d0f28f6490a045c8af8e29ac6ec113c50696d
Bruno <info@proyectarse.com>
2021-07-03 12:41:16 -0400

    UPDATE: payment

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,10 @@
 const functions = require('firebase-functions');
 const { geocodeRequest } = require('./geocode');
 const { placesRequest } = require('./places');
-// const { payRequest } = require('./pay');
+const { payRequest } = require('./pay');
 
 const { Client } = require('@googlemaps/google-maps-services-js');
-// const stripeClient = require('stripe')(functions.config().stripe.key);
+const stripeClient = require('stripe')(functions.config().stripe.key);
 const googleClient = new Client({});
 
 exports.geocode = functions.https.onRequest((request, response) => {
@@ -15,6 +15,6 @@ exports.placesNearby = functions.https.onRequest((request, response) => {
 	placesRequest(request, response, googleClient);
 });
 
-// exports.pay = functions.https.onRequest((request, response) => {
-// 	payRequest(request, response, stripeClient);
-// });
+exports.pay = functions.https.onRequest((request, response) => {
+	payRequest(request, response, stripeClient);
+});
--- src/services/checkout/checkout.service.js
+++ src/services/checkout/checkout.service.js
@@ -1,24 +1,26 @@
-import createStripe from "stripe-client";
-import { host } from "../../utils/env";
+import createStripe from 'stripe-client';
+import { host } from '../../utils/env';
 
 const stripe = createStripe(
-  "pk_test_51HzDwlIfEuMDL6nvAXXM3pbywxTyQjerlEGqAyg8sUESBJKtAy6j86uMoCvYLInPpahyRSC3S8G65md8jbkGYPNE002bZm2QZf"
+	'pk_test_51J98o9Hmla3kUYWsPBn6ldfFhj773h00fYdazxvT0vlAhylPYXEhSTsdbNarIneM7aM1yMxalbyDnCQ8HsLRK14W00V1pv8FDx'
 );
 
 export const cardTokenRequest = (card) => stripe.createToken({ card });
 
 export const payRequest = (token, amount, name) => {
-  return fetch(`${host}/pay`, {
-    body: JSON.stringify({
-      token,
-      name,
-      amount,
-    }),
-    method: "POST",
-  }).then((res) => {
-    if (res.status > 200) {
-      return Promise.reject("something went wrong processing your payment");
-    }
-    return res.json();
-  });
+	return fetch(`${host}/pay`, {
+		body: JSON.stringify({
+			token,
+			name,
+			amount,
+		}),
+		method: 'POST',
+	}).then((res) => {
+		if (res.status > 200) {
+			return Promise.reject(
+				'something went wrong processing your payment'
+			);
+		}
+		return res.json();
+	});
 };
